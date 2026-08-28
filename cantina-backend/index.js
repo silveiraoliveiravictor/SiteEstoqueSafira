cc4d6b7a27023458ce2744737e470f24233b50bd
Sophia C. S. <sophia.cabral.soares@escola.pr.gov.br>
2026-08-28 13:11:28 -0300

    Adicionando biblioteca dotenv.

--- cantina-backend/index.js
+++ cantina-backend/index.js
@@ -6,12 +6,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+require('dotenv').config();
+
 const pool = new Pool({
-    user: 'sosoBanana',
-    host: 'localhost',
-    database: 'psqlCantina',
-    password: 'barchen',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
 // ==========================================
